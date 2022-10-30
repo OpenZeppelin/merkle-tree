044b927c0ee0d3bbd5c3ffdf07dc8c1a6fdd8812
Francisco Giordano <frangio.1@gmail.com>
2022-10-30 20:53:49 -0300

    add property test for multiproofs

--- src/core.test.ts
+++ src/core.test.ts
@@ -1,25 +1,43 @@
 import fc from 'fast-check';
 import { equalsBytes, hexToBytes } from 'ethereum-cryptography/utils';
-import { makeMerkleTree, getProof, processProof } from './core';
+import { makeMerkleTree, getProof, processProof, getMultiProof, processMultiProof } from './core';
 import { hex } from './bytes';
 
 const leaf = fc.uint8Array({ minLength: 32, maxLength: 32 }).map(x => PrettyBytes.from(x));
 const leaves = fc.array(leaf, { minLength: 1 });
 const leavesAndIndex = leaves.chain(xs => fc.tuple(fc.constant(xs), fc.nat({ max: xs.length - 1 })));
+const leavesAndIndices = leaves.chain(xs => fc.tuple(fc.constant(xs), fc.uniqueArray(fc.nat({ max: xs.length - 1 }))));
 
 fc.configureGlobal({ numRuns: 10000 });
 
 describe('properties', () => {
-  it('each leaf of a tree is provable', () => {
+  it('a leaf of a tree is provable', () => {
     fc.assert(
       fc.property(leavesAndIndex, ([leaves, leafIndex]) => {
         const tree = makeMerkleTree(leaves);
         const root = tree[0];
         if (root === undefined) return false;
-        const treeIndex = tree.length - leafIndex - 1;
+        const treeIndex = tree.length - 1 - leafIndex;
         const proof = getProof(tree, treeIndex);
         const leaf = leaves[leafIndex]!;
         const impliedRoot = processProof(leaf, proof);
+        return equalsBytes(root, impliedRoot);
+      }),
+    );
+  });
+
+  it('a subset of leaves of a tree are provable', () => {
+    fc.assert(
+      fc.property(leavesAndIndices, ([leaves, leafIndices]) => {
+        fc.pre(leaves.length > 1); // TODO: fix test for leaves.length = 1
+        const tree = makeMerkleTree(leaves);
+        const root = tree[0];
+        if (root === undefined) return false;
+        leafIndices.sort((a, b) => a - b);
+        const treeIndices = leafIndices.map(i => tree.length - 1 - i);
+        const proof = getMultiProof(tree, treeIndices);
+        const provenLeaves = leafIndices.map(i => leaves[i]!);
+        const impliedRoot = processMultiProof(provenLeaves, proof);
         return equalsBytes(root, impliedRoot);
       }),
     );
