54245e3753bd2e232c4381e2b8aa9138c585a295
Hadrien Croubois <hadrien.croubois@gmail.com>
2023-12-18 10:35:42 +0100

    add missing file

---
+++ src/options.ts
@@ -0,0 +1,14 @@
+// MerkleTree building options
+export type MerkleTreeOptions = Partial<{
+  sortLeaves: boolean;
+}>;
+
+// For backward compatibility reasons, leaves are sorted by default.
+// This can be disabled for usecases where leaves ordering needs to be preserved
+export const defaultOptions: Required<MerkleTreeOptions> = {
+  sortLeaves: true,
+};
+
+export function withDefault(options: MerkleTreeOptions) : Required<MerkleTreeOptions> {
+  return { ...defaultOptions, ...options };
+}
