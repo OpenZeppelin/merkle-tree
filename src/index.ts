ec13d683de7b3e24963d4201630522ae5cd57236
Hadrien Croubois <hadrien.croubois@gmail.com>
2024-01-10 13:21:50 +0100

    check index.ts exports

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,5 @@
+export type { MerkleTreeData } from './format';
+export type { MerkleTreeOptions } from './options';
+
 export { SimpleMerkleTree } from './simple';
 export { StandardMerkleTree } from './standard';
