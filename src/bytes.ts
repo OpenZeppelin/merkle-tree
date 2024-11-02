dae516dffee50b4d9bf8fdd89dfbf53bf0609b9b
Francisco Giordano <fg@frang.io>
2024-11-02 12:03:47 -0300

    ensure backwards compatibility of type signatures

--- src/bytes.ts
+++ src/bytes.ts
@@ -1,12 +1,33 @@
-import type { BytesLike } from '@metamask/utils';
-type HexString = `0x${string}`;
+type Bytes = Uint8Array;
+type BytesLike = ArrayLike<number> | string;
+type HexString = string;
 
-import { createBytes as toBytes, createHex as toHex, concatBytes as concat } from '@metamask/utils';
+import { createBytes, createHex, concatBytes, type BytesLike as StrictBytesLike } from '@metamask/utils';
+
+function toBytes(value: BytesLike): Bytes {
+  if (Array.isArray(value)) {
+    return createBytes(new Uint8Array(value));
+  } else {
+    return createBytes(value as StrictBytesLike);
+  }
+}
+
+function toHex(value: BytesLike): `0x${string}` {
+  if (Array.isArray(value)) {
+    return createHex(new Uint8Array(value));
+  } else {
+    return createHex(value as StrictBytesLike);
+  }
+}
+
+function concat(values: BytesLike[]): Bytes {
+  return concatBytes(values.map(toBytes));
+}
 
 function compare(a: BytesLike, b: BytesLike): number {
   const diff = BigInt(toHex(a)) - BigInt(toHex(b));
   return diff > 0 ? 1 : diff < 0 ? -1 : 0;
 }
 
-export type { HexString, BytesLike };
+export type { HexString, BytesLike, Bytes };
 export { toBytes, toHex, concat, compare };
